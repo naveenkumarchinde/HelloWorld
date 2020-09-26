a2a09a853e7d086a6cb8759e7f5baaa4ea0bfc68
naveenkumarchinde <71186580+naveenkumarchinde@users.noreply.github.com>
2020-09-26 21:41:46 +0530

    Add files via upload

---
+++ src/main/java/com/mindtree/HelloWorld.java
@@ -0,0 +1,8 @@
+package com.mindtree;
+
+public class HelloWorld {
+	
+public static void main(String[] args) {
+	System.out.println("***SYSOUT used instead of LOGGER");
+}
+}
---
+++ src/test/java/com/mindtree/JUnitSampleTest.java
@@ -0,0 +1,31 @@
+package com.mindtree;
+
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
+public class JUnitSampleTest {
+
+	private String verify = "nk";
+
+	@Test
+	public void instanceOfTest() {
+		assertThat(new ArrayList<>(), instanceOf(List.class));
+	}
+
+	@Test
+	public void assertTrueTest() {
+		assertTrue(true);
+	}
+
+	@Test
+	public void equalToTest() {
+		assertThat(verify, is("nk"));
+	}
+}
