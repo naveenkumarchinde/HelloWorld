716f7fa3f012f5171ae52e6e836585734687c90c
naveenkumarchinde <71186580+naveenkumarchinde@users.noreply.github.com>
2020-09-27 00:52:57 +0530

    Update HelloWorld.java

--- src/main/java/com/mindtree/HelloWorld.java
+++ src/main/java/com/mindtree/HelloWorld.java
@@ -3,6 +3,7 @@ package com.mindtree;
 public class HelloWorld {
 	
 public static void main(String[] args) {
-	System.out.println("***SYSOUT used instead of LOGGER");
+	System.out.println("***SYSOUT used instead of LOGGER-1");
+	System.out.println("***SYSOUT used instead of LOGGER-2");
 }
 }
