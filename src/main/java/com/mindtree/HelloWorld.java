36932a0dd2b1431b3a5769b5757ccd9c97cb9595
naveenkumarchinde <71186580+naveenkumarchinde@users.noreply.github.com>
2020-09-27 17:03:13 +0530

    Update HelloWorld.java
    resolved build issue

--- src/main/java/com/mindtree/HelloWorld.java
+++ src/main/java/com/mindtree/HelloWorld.java
@@ -3,7 +3,7 @@ package com.mindtree;
 public class HelloWorld {
 	
 public static void main(String[] args) {
-	creating build fail scenario
+	
 	System.out.println("***SYSOUT used instead of LOGGER-1");
 	
 }
