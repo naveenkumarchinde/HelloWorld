d78b801029dea193cadf18a4df0ffff6dcc2bd81
naveenkumarchinde <71186580+naveenkumarchinde@users.noreply.github.com>
2020-09-27 18:14:30 +0530

    Update HelloWorld.java

--- src/main/java/com/mindtree/HelloWorld.java
+++ src/main/java/com/mindtree/HelloWorld.java
@@ -3,7 +3,7 @@ package com.mindtree;
 public class HelloWorld {
 	
 public static void main(String[] args) {
-
+creating build fail screnario to trigger Email notification
 	System.out.println("***SYSOUT used instead of LOGGER-1");
 	
 }
